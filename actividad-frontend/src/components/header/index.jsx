a7a0aaffaf30fd675c5c512c81a643b0d3b38655
Joel L. Ruíz Carrillo <luruicar@upv.edu.es>
2023-04-19 17:21:41 +0200

    image user

--- actividad-frontend/src/components/header/index.jsx
+++ actividad-frontend/src/components/header/index.jsx
@@ -1,22 +1,27 @@
 import zaraLogo from "../../assets/images/zara-logo.svg";
 import { Placeholder } from "../placeholder";
+import icono from "../../assets/images/icono.png";
 import "./style.css";
 
 export const Header = () => {
+  const onClick = () => {
+    window.location.reload();
+  };
 
-    const onClick = () => {
-        window.location.reload();
-    }
-
-    return (
-        <div className="header">
-            <div className="header__logo">
-                <img src={zaraLogo} alt="Zara" data-testid="zara-logo" onClick={onClick} />
-            </div>
-            <div className="header__avatar">
-                {/* TODO: Exercise 1 - Replace the Placeholder component with our Avatar component */}
-                <Placeholder width={30} height={30} />
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="header">
+      <div className="header__logo">
+        <img
+          src={zaraLogo}
+          alt="Zara"
+          data-testid="zara-logo"
+          onClick={onClick}
+        />
+      </div>
+      <div className="header__avatar">
+        {/* TODO: Exercise 1 - Replace the Placeholder component with our Avatar component */}
+        <img src={icono} width={30} height={30} />
+      </div>
+    </div>
+  );
+};
