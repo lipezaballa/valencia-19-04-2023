af418fbccd55bc662347821afc44dc3647b50dd9
Joel L. Ruíz Carrillo <luruicar@upv.edu.es>
2023-04-19 18:29:57 +0200

    imagenes

--- actividad-frontend/src/app.jsx
+++ actividad-frontend/src/app.jsx
@@ -1,24 +1,26 @@
 import { Header } from "./components/header";
 import { Placeholder } from "./components/placeholder";
 import { Filter } from "./components/Ranking/Filter";
+import { Ranking } from "./components/Ranking/ranking";
 import "./app.css";
 import { Summary } from "./components/summary/summary";
 
 function App() {
-    return (
-        <div className="app">
-            <Header />
-            <div className="container">
-                <div className="summary">
-                    <Summary />
-                </div>
-                <div className="ranking">
-                    {/* TODO: Exercise 3 - Create the ranking component */}
-                    <Filter />
-                </div>
-            </div>
+  return (
+    <div className="app">
+      <Header />
+      <div className="container">
+        <div className="summary">
+          <Summary />
         </div>
-    );
+        <div className="ranking">
+          {/* TODO: Exercise 3 - Create the ranking component */}
+          <Filter />
+          <Ranking></Ranking>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- actividad-frontend/src/components/Ranking/ranking/index.jsx
+++ actividad-frontend/src/components/Ranking/ranking/index.jsx
@@ -1,3 +1,49 @@
-import { dress_a } from "";
+import { useEffect, useState } from "react";
+import "./style.css";
 
-export const ranking = () => {};
+export const Ranking = ({ width, height }) => {
+  const [summary, setSummary] = useState({
+    totalOrders: 0,
+    totalProducts: 0,
+    totalSales: 0,
+  });
+
+  async function logJSONData() {
+    const response = await fetch("/report-summary");
+    const jsonData = await response.json();
+
+    setSummary(() => {
+      return jsonData;
+    });
+    console.log(jsonData.totalOrders);
+    console.log(jsonData);
+    console.log(summary);
+  }
+
+  useEffect(() => {
+    logJSONData();
+  }, []);
+
+  return (
+    <div className="products">
+      <img
+        src={"/assets/products/dress_a.jpg"}
+        style={{ height: 500, width: 300, gap: 50 }}
+      />
+
+      <img
+        src={"/assets/products/jacket_a.jpg"}
+        style={{ height: 500, width: 300, gap: 50 }}
+      />
+      <img
+        src={"/assets/products/jacket_b.jpg"}
+        style={{ height: 500, width: 300 }}
+      />
+
+      <img
+        src={"/assets/products/pant_a.jpg"}
+        style={{ height: 500, width: 300 }}
+      />
+    </div>
+  );
+};
